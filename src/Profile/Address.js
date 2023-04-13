5f17c713737b3ecf99fda17f985d32afeb029bc9
mohamed <mohamedhadd95@gmail.com>
2023-04-13 23:31:44 +0100

    my first commit

---
+++ src/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Address() {
+  return (
+    <div className='adr'>
+      <h3>Ksibet Mediouni/Monastir </h3>
+    </div>
+  );
+}
+
+export default Address;
---
+++ src/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import img from '../assets/1655378995842.jpg'
+
+function ProfilePhoto() {
+  return (
+    <div className='prf'>
+      <img src={img} alt="My Profile" />
+    </div>
+  );
+}
+
+export default ProfilePhoto;
